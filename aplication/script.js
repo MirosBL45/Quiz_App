28eb2ef0c9354749c00ea9d626e4758b50d383d7
Miroslav Jovic <jovicmiroslav88@gmail.com>
2022-06-18 23:49:58 +0200

    initial commit

---
+++ aplication/script.js
@@ -0,0 +1,118 @@
+const quizData = [
+    {
+        question: 'How old is Ruby the cat?',
+        a: '1 year',
+        b: '3 years',
+        c: '2 years',
+        d: '4 years',
+        correctAns: 'c'
+    }, {
+        question: 'What is the most used programming language in 2019?',
+        a: 'Java',
+        b: 'C',
+        c: 'C#',
+        d: 'JavaScript',
+        correctAns: 'd'
+    }, {
+        question: 'Who is the selector of Serbia in football?',
+        a: 'Miroslav Jovic',
+        b: 'Dragan Stojkovic Piksi',
+        c: 'Zeljko Obradovic',
+        d: 'Mateja Kezman',
+        correctAns: 'b'
+    }, {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Basketball',
+        c: 'Aplication interface',
+        d: 'Helicopters',
+        correctAns: 'a'
+    }, {
+        question: 'What year was JavaScript launched?',
+        a: '1994',
+        b: '1996',
+        c: '1995',
+        d: 'None above',
+        correctAns: 'c'
+    }
+]
+
+
+//get elements from HTML
+const quiz = document.querySelector('#quizMain');
+const odgovoriEls = document.querySelectorAll('input[type="radio"]');
+const questionEl = document.querySelector("#question")
+const a_text = document.querySelector('#a_text');
+const b_text = document.querySelector('#b_text');
+const c_text = document.querySelector('#c_text');
+const d_text = document.querySelector('#d_text');
+const submitBtn = document.querySelector('button');
+
+
+//set global variables
+let trenutnoPitanje = 0;
+let rezultat = 0;
+
+
+
+//call function for load quiz
+ucitajSledecePitanje();
+
+
+//function for load quiz
+function ucitajSledecePitanje() {
+    deselectAnswer();
+    //current question
+    const trenutniPodatakPitanja = quizData[trenutnoPitanje];
+
+    questionEl.innerText = trenutniPodatakPitanja.question;
+    a_text.innerText = trenutniPodatakPitanja.a;
+    b_text.innerText = trenutniPodatakPitanja.b;
+    c_text.innerText = trenutniPodatakPitanja.c;
+    d_text.innerText = trenutniPodatakPitanja.d;
+}
+
+
+//function for taking answer
+function uzmiObelezen() {
+    let odgovor = undefined;
+
+    odgovoriEls.forEach((odgovorEl) => {
+        if (odgovorEl.checked) {
+            odgovor = odgovorEl.id;
+        }
+    });
+
+    return odgovor;
+}
+
+
+//deselect next answer
+function deselectAnswer() {
+    odgovoriEls.forEach((odgovorEl) => {
+        odgovorEl.checked = false;
+    });
+}
+
+
+//listener for submit button
+submitBtn.addEventListener('click', () => {
+
+    const odgovorObelezen = uzmiObelezen();
+
+    if (odgovorObelezen) {
+        if (odgovorObelezen === quizData[trenutnoPitanje].correctAns) {
+            rezultat++;
+        }
+        trenutnoPitanje++
+        if (trenutnoPitanje < quizData.length) {
+            ucitajSledecePitanje();
+        } else {
+            quiz.innerHTML = `
+                <h2 class="finished">You answered correctly at ${rezultat}/${quizData.length} questions!!!</h2>
+                
+                <button onclick="location.reload()">Do it again!</button>
+            `;
+        }
+    }
+})
---
+++ readme.md
@@ -0,0 +1,13 @@
+This is Quiz_App I saw on https://www.youtube.com/watch?v=dtKciwk_si4,
+thanks a lot to Florin Pop for that video
+
+I am still learning JavaScript, this mini-project I found on Florin Pop YouTube channel, and it was interesting to me, so I did it on my own
+
+Also, it is done with SASS, it is fun way to do CSS
+
+Any advice is wellcome :)
+
+Used for colors:
+https://www.eggradients.com/
+
+https://flatuicolors.com/palette/au
